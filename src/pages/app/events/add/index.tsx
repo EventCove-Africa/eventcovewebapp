048a4c9468ac212b60dbe6951577b4173275547f
Moses <ojeimoses@gmail.com>
2025-01-18 08:52:31 +0100

    Updates...

--- src/pages/app/events/add/index.tsx
+++ src/pages/app/events/add/index.tsx
@@ -5,7 +5,7 @@ export default function AddEvents() {
   const navigate = useNavigate();
   return (
     <div className="w-full h-full">
-      <h3 className="text-dark_200 text-base font-normal flex gap-1 items-center mb-3">
+      <h3 className="text-dark_200 md:text-base text-sm font-normal flex gap-1 md:items-center items-start mb-3">
         <ArrowLeft
           className="cursor-pointer"
           size="16"
@@ -14,7 +14,7 @@ export default function AddEvents() {
         />
         Event vibes loading—fill out the form to get started! 🌟
       </h3>
-      <div className="w-1/2 bg-white h-[500px] rounded-xl shadow">
+      <div className="lg:w-1/2 w-full bg-white h-[500px] rounded-xl shadow">
 
       </div>
     </div>
